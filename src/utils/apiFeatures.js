14d06868f7531bd25fccfac9588eea8cd7d7fb2d
Ayush Batra <ayushbatra128@gmail.com>
2022-07-06 00:38:21 +0530

    search feature added

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -5,7 +5,7 @@ class ApiFeatures {
   }
 
   search() {
-    const keyword = this.queryStr.keyword
+    const keyword = this.queryStr.q
       ? {
           title: {
             $regex: this.queryStr.keyword,
